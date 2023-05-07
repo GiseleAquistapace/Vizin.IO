d39f8c441b1ce3093e2cc83c01964331705c5a99
GiseleAquistapace <125749367+GiseleAquistapace@users.noreply.github.com>
2023-05-07 16:46:31 -0300

    Ajuste pg inicial

--- inicial.php
+++ inicial.php
@@ -6,17 +6,22 @@ include_once 'conexao.php'
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="style.css"> 
     <title>Vizin.IO - Conectando Vizinhos</title>      
   </head>
   <body>  
+  
     <div class="welcome">
-      <img id="logo2" src="Image/logo.png">
+      <img id="logo2" src="Image/logo.png">      
+      <img id="pin" src="Image/pin.png">
+      <img id = "emg" src="Image/emergencia.png">
+      <img id = "cht" src="Image/chat.png">
+      <img id = "doa" src="Image/doacao.png">
       <h1>Vizin.IO</h1>           
-      <h2>Bem-vindo(a) <?php echo $_SESSION['nome']; ?> !</h2>
-      <button id="verificarAlertas">Verificar Alertas</button>
+      <h2>Bem-vindo(a) <?php echo utf8_encode($_SESSION['nome']); ?>!</h2>      
+      <button id="verificarAlertas">Verificar Alertas</button>      
       <button id="contatarEmergencia">Contatar Emergência</button>
       <button id="chatBairro">Chat do Bairro</button>
       <button id="contribuicoes">Contribuições</button>
@@ -24,3 +29,4 @@ include_once 'conexao.php'
     <script src="app.js"></script>
   </body>
 </html>
+
